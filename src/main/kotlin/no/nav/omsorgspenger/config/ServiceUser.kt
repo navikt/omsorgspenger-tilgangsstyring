ad3e4acf78cda15723e75b878e44c1a75be51385
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-12-03 14:19:10 +0100

    Lagt til prod-config

--- src/main/kotlin/no/nav/omsorgspenger/config/ServiceUser.kt
+++ src/main/kotlin/no/nav/omsorgspenger/config/ServiceUser.kt
@@ -1,10 +1,6 @@
 package no.nav.omsorgspenger.config
 
-import java.util.Base64
-
 data class ServiceUser(
     val username: String,
     val password: String
-) {
-    val basicAuth = "Basic ${Base64.getEncoder().encodeToString("$username:$password".toByteArray(Charsets.UTF_8))}"
-}
+)
