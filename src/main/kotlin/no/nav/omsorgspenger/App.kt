5fad3da8ca5b49ec57f6664411d072d7b3564d3c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-04-19 14:52:49 +0200

    Disabe FAIL_ON_UNKNOWN_PROPERTIES

--- src/main/kotlin/no/nav/omsorgspenger/App.kt
+++ src/main/kotlin/no/nav/omsorgspenger/App.kt
@@ -1,5 +1,6 @@
 package no.nav.omsorgspenger
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -66,6 +67,7 @@ fun Application.app() {
 
     val objectMapper: ObjectMapper = jacksonObjectMapper()
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
     val pdlConfig = environment.config.config("nav.pdl")
     val azureConfig = environment.config.config("nav.auth.azure")
