3478bdc74b404c6be9355b1e8c18e234b3f2b920
albrektsson <emil.albrektsson@nav.no>
2021-12-20 12:56:29 +0100

    Java 17 + dependency bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfVersion = "3.1.6.4-e07c5ec"
-val ktorVersion = "1.6.5"
-val junitJupiterVersion = "5.8.1"
+val dusseldorfVersion = "3.1.6.7-7d29e37"
+val ktorVersion = "1.6.7"
+val junitJupiterVersion = "5.8.2"
 val assertJVersion = "3.21.0"
-val mockkVersion = "1.12.0"
+val mockkVersion = "1.12.1"
 
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
@@ -57,11 +57,11 @@ repositories {
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "16"
+        kotlinOptions.jvmTarget = "17"
     }
 
     named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
-        kotlinOptions.jvmTarget = "16"
+        kotlinOptions.jvmTarget = "17"
     }
 
     withType<Test> {
@@ -85,6 +85,6 @@ tasks {
 
 
     withType<Wrapper> {
-        gradleVersion = "7.2"
+        gradleVersion = "7.3.2"
     }
 }
