3b066035c4ad7272f8b799883bfd86617e69ef92
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-03-14 14:20:38 +0100

    Erstattet ghcr med gar, dep bumps, sonarcloud, ny dev ingress (#119)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfVersion = "3.2.2.2-d9c7672"
-val ktorVersion = "2.2.2"
-val junitJupiterVersion = "5.9.1"
-val assertJVersion = "3.24.1"
-val mockkVersion = "1.13.3"
+val dusseldorfVersion = "3.2.2.4-98ccf55"
+val ktorVersion = "2.2.4"
+val junitJupiterVersion = "5.9.2"
+val assertJVersion = "3.24.2"
+val mockkVersion = "1.13.4"
 
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.0"
+    id("org.sonarqube") version "4.0.0.2929"
+    jacoco
 }
 
 java {
@@ -83,6 +85,28 @@ tasks {
 
 
     withType<Wrapper> {
-        gradleVersion = "7.6"
+        gradleVersion = "8.0.2"
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "navikt_omsorgspenger-tilgangsstyring")
+        property("sonar.organization", "navikt")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.sourceEncoding", "UTF-8")
     }
 }
